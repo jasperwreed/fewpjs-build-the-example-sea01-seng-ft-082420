675c6d6ef5af17d613bb067b754a20008cc5b969
jasperwreed <jmixon.reed@gmail.com>
2020-10-08 11:02:04 -0700

    basically done lol

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likes = document.querySelectorAll('.like')
+const modal = document.getElementById('modal')
 
-// Your JavaScript code goes here!
+// Hiding error modal
+modal.className = 'hidden'
 
+// Server Call && Change Heart Status
+const serverCall = (e) => {
+  let heart = e.target
+  mimicServerCall()
+  .then(serverMessage => {
+    alert('Server Notified')
+    alert(serverMessage)
+    if (heart.className === 'activated-heart') {
+      heart.className = ''
+      heart.innerText = EMPTY_HEART
+    } else {
+      heart.className = 'activated-heart'
+      heart.innerText = FULL_HEART
+    }
+  })
+  .catch(error => {
+    // window.setTimeout(modal.className = 'hidden', 5000)
+    modal.className = ''
+    alert(error)
+  })
+}
 
+// Event Listener
 
+for (const glyph of likes) {
+  glyph.addEventListener('click', (e) => serverCall(e))
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
